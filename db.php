0e339655678737aaf40bdca40cee1319c5f377ec
gamaygeo10 <geovincebartulin@gmail.com>
2025-07-18 21:04:01 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'mythological-creatures.mysql.database.azure.com  
-$dbname = 'mythological_creatures'; 
+$host = 'mythological-creatures.mysql.database.azure.com';  // Your Azure MySQL server hostname
+$dbname = 'mythological_creatures';  // Your database name
 $username = 'user_admin@mythological-creatures';  // Your MySQL username (with server name)
-$password = 'Polipopo07!';
+$password = 'Polipopo07!';  // Your MySQL password
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
